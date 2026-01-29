54dde2dd0bca1615c9c2a4e7ef4b99c17c4ea7e9
Isaac <hi@isaac.sh>
2026-01-29 00:21:40 -0800

    Fix Nix flake: use new Darwin SDK pattern (apple-sdk_15)

--- flake.nix
+++ flake.nix
@@ -24,13 +24,10 @@
 
         isDarwin = pkgs.stdenv.isDarwin;
 
-        darwinDeps = with pkgs; pkgs.lib.optionals isDarwin [
-          darwin.apple_sdk.frameworks.Security
-          darwin.apple_sdk.frameworks.CoreServices
-          darwin.apple_sdk.frameworks.CoreFoundation
-          darwin.apple_sdk.frameworks.AppKit
-          darwin.apple_sdk.frameworks.WebKit
-          darwin.libobjc
+        # New Darwin SDK pattern - just include the SDK, frameworks are auto-available
+        darwinDeps = pkgs.lib.optionals isDarwin [
+          pkgs.apple-sdk_15
+          pkgs.libiconv
         ];
 
         linuxDeps = with pkgs; pkgs.lib.optionals (!isDarwin) [
@@ -59,37 +56,6 @@
           '' + pkgs.lib.optionalString (!isDarwin) ''
             export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath linuxDeps}:$LD_LIBRARY_PATH"
           '';
-        };
-
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "aeromessage";
-          version = "0.1.14";
-          src = ./oxidized;
-          cargoLock.lockFile = ./oxidized/Cargo.lock;
-
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            nodejs_20
-          ] ++ pkgs.lib.optionals isDarwin [ pkgs.xcbuild ];
-
-          buildInputs = commonDeps ++ darwinDeps ++ linuxDeps;
-
-          # Skip Tauri bundling in Nix build - just build the binary
-          buildPhase = ''
-            cargo build --release
-          '';
-
-          installPhase = ''
-            mkdir -p $out/bin
-            cp target/release/aeromessage $out/bin/
-          '';
-
-          meta = with pkgs.lib; {
-            description = "Batch-reply to iMessages, beautifully";
-            homepage = "https://github.com/slightknack/aeromessage";
-            license = licenses.cc0;
-            platforms = platforms.darwin;  # macOS only (iMessage)
-          };
         };
       });
 }
